42a7c30a0282d018cf2ca9e8961ec50a94376aa0
Arthur <arthur.h.f13@gmail.com>
2020-12-12 22:15:49 -0300

    nothing important

--- MainClass2.java
+++ MainClass2.java
@@ -3,5 +3,8 @@ public class MainClass{
   public static void main(String args[]) { 
     Scanner _key = new Scanner(System.in); 
     Integer a;
+    Integer b;
     a = 1.1;
+    a = 2;
+    b = 6;
   }}
